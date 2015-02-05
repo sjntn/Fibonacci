d16147040612092f11b145324d8c5a843d654edb
Ely De La Cruz <elycruz@elycruz.com>
2015-02-04 22:33:02 -0500

    Amended comments in javascript version.

--- fibonacci.js
+++ fibonacci.js
@@ -5,30 +5,18 @@
  * @returns {Array}
  */
 function fib (limit) {
-
-    // If no limit was passed in use 1000 as the default
     limit = limit || 1000;
-
-    // Preliminaries
     var a = 0,
         b = 1,
         out = [];
-
-    // While less than limit
     while (a <= limit) {
         out.push(a);
         out.push(b);
         a = a + b;
         b = a + b;
     }
-
-    // Remove last item in array
-    // since it will be above limit in this setup
     out.pop();
-
-    // Return our array
     return out;
 }
 
-// Print the fib series comma separated
 console.log( fib(1000).join(', ') );
