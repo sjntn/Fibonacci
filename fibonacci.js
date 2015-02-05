7bd4feef9c25868a6ae6e1774319291433756bd1
Ely De La Cruz <elycruz@elycruz.com>
2015-02-04 22:20:51 -0500

    Fibonacci series with javascript (with comments :-)).

---
+++ fibonacci.js
@@ -0,0 +1,35 @@
+/**
+ * Created by elycruz on 2/4/15.
+ * Fibonacci function for getting an array of numbers
+ * representing the fibonacci series up to a passed in limit.
+ * @param limit {Number}
+ * @returns {Array}
+ */
+function fib (limit) {
+
+    // If no limit was passed in use 1000 as the default
+    limit = limit || 1000;
+
+    // Preliminaries
+    var a = 0,
+        b = 1,
+        out = [];
+
+    // While less than limit
+    while (a <= limit) {
+        out.push(a);
+        out.push(b);
+        a = a + b;
+        b = a + b;
+    }
+
+    // Remove last item in array
+    // since it will be above limit in this setup
+    out.pop();
+
+    // Return our array
+    return out;
+}
+
+// Print the fib series comma separated
+console.log( fib(1000).join(', ') );
